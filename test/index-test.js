91bee363f0b00142d1128cf1553a4729542cf656
James Min <jmin499@gmail.com>
2020-10-12 16:36:32 -0400

    mocha chai tests dom

--- test/index-test.js
+++ test/index-test.js
@@ -1,17 +1,34 @@
-var assert = require('assert');
-var hello = require('../index');
+const { expect } = require('chai');
+const chai = require('chai')
+const hello = require('../index');
+const { JSDOM } = require('jsdom');
+
+chai.use(require('chai-dom'))
 require('jsdom-global')()
 
 
 describe('Hello World', () => {
   it('hello function should return hello world', () => {
-    assert.equal(hello(), 'hello world')
+    expect(hello()).to.equal('hello world')
   })
 })
 
-describe('Heading', () => {
-  it("DOM should have a h1 element that saids Hello World!'", () => {
-    let element = document.getElementsByClassName('h1')[0];
-    assert.equal(element.textContent, 'Hello World!')
+
+describe('HTML', () => {
+
+  beforeEach((done) => {
+    JSDOM.fromFile('index.html')
+    .then((dom) => {
+      global.document = dom.window.document
+    })
+    .then(done, done);
+  });
+
+  describe('Heading', () => {
+    it("DOM should have a h1 element that saids Hello World!'", () => {
+      let element = document.getElementById('hello')
+      expect(element).to.have.text('hello world')
+    })
   })
+ 
 })
