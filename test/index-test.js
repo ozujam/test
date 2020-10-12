f4b7c2b57d0d27b229ebbb72eb0b2433f2bbcb89
James Min <jmin499@gmail.com>
2020-10-12 13:44:51 -0400

    installed jsdom into Node

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,17 @@
 var assert = require('assert');
-var helloWorld = require('../index')
+var hello = require('../index');
+require('jsdom-global')()
 
 
 describe('Hello World', () => {
-  it('helloWorld function should return hello world', () => {
-    assert.equal(helloWorld(), 'hello world')
+  it('hello function should return hello world', () => {
+    assert.equal(hello(), 'hello world')
+  })
+})
+
+describe('Heading', () => {
+  it("DOM should have a h1 element that saids Hello World!'", () => {
+    let element = document.getElementsByClassName('h1')[0];
+    assert.equal(element.textContent, 'Hello World!')
   })
 })
